0340e95457c9e14629c3130c79790e8ed8b19ef6
Unknown <kjk464@humboldt.edu>
2018-09-24 17:16:53 -0700

    integrate yelp api; do housekeeping

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var path = require("path");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
+require("dotenv").config();
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-
 app.use(express.static("public"));
 
 // Bodyparser for automatically parsing incoming JSON
@@ -16,9 +17,9 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers");
-
-app.use(routes);
+// link server routing data
+require("./routes/api")(app);
+require("./routes/html")(app, path);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
