22ea7294e23e087e2574e0bdc4173cfe295831ef
Matthew Amis <mfamis@gmail.com>
2018-09-24 22:45:42 -0700

    Updated email login to use ORM.

--- config/passport.js
+++ config/passport.js
@@ -83,19 +83,18 @@ module.exports = function(passport) {
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
         function(req, username, password, done) { // callback with email and password from our form
-            connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
-                if (err)
-                    return done(err);
-                if (!rows.length) {
+            //connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
+            db.User.findOne({ where: { username: username } }).then( user => { 
+                if (!user) {
                     return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
                 }
 
                 // if the user is found but the password is wrong
-                if (!bcrypt.compareSync(password, rows[0].password))
+                if (!bcrypt.compareSync(password, user.password))
                     return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
                 // all is well, return successful user
-                return done(null, rows[0]);
+                return done(null, user);
             });
         })
     );
